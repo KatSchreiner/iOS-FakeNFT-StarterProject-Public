f93c70587f052c19703e06982ca191691867ac4b
Kamila <ksalimzh@gmail.com>
2024-09-11 16:31:00 +0500

    Добавила статистику вьюшку в  tab bar controller scene с соответствующей иконкой + localizable string for statistics

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -9,6 +9,12 @@ final class TabBarController: UITabBarController {
         image: UIImage(systemName: "square.stack.3d.up.fill"),
         tag: 0
     )
+    
+    private let statisticsTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.statistics", comment: ""),
+        image: UIImage(named: "statistics_tb"),
+        tag: 1
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,9 +22,13 @@ final class TabBarController: UITabBarController {
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
+        
+        let statisticsViewController = UIViewController() // to do: change to real scene impl
+        
         catalogController.tabBarItem = catalogTabBarItem
+        statisticsViewController.tabBarItem = statisticsTabBarItem
 
-        viewControllers = [catalogController]
+        viewControllers = [catalogController, statisticsViewController]
 
         view.backgroundColor = .systemBackground
     }
